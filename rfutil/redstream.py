bd4a866017632f79f044fc54d616fbda476a568e
kouritron <kouritron@users.noreply.github.com>
2017-02-25 06:46:16 -0500

    redfile arkiver now makes 2 red copies, the random ones need to be added

--- rfutil/redstream.py
+++ rfutil/redstream.py
@@ -17,6 +17,8 @@ class RedStream:
     # 129 == 0x81
     FLAG_BYTE = 129
 
+    REDUNDANCY_LEVEL = 4
+
     # class variables:
     # presumably the user will set this to something like 512 or 4k as the device min block size
     # we want this to be large enough such that the header overhead is about 10% or less, but not too large
@@ -86,9 +88,9 @@ class RedStream:
         """
         """
 
-        # print self.READ_SIZE
+        print "--------------------------------------------------------------------------------------------------------"
+        print "redundantize_and_save called on input file: " + in_file_name
 
-        bytes_written = 0
 
         # open in and out files for reading in binary modem and writing in binary mode
         infile = open(in_file_name, "rb")
@@ -100,37 +102,84 @@ class RedStream:
         infile.seek(0, 0)
 
         print "source file appears to be: " + str(infile_size) + " bytes"
+        infile_size_rounded_up = 0
+
+        if infile_size % self.READ_SIZE == 0:
+            infile_size_rounded_up = infile_size
+        else:
+            # // is unconditionally the integer division with truncate operator.
+            infile_size_rounded_up = ((infile_size // self.READ_SIZE) + 1) * self.READ_SIZE
+
+        print "source file after getting packet size aligned is: " + str(infile_size_rounded_up) + " bytes"
+
+        total_packet_count = infile_size_rounded_up // self.READ_SIZE
+        print "i believe i will need " + str(total_packet_count) + " packets in total"
+
+        source_offset = 0
+        packets_written = 0
 
         # u can append an int (0,255) or extend using a bytes object.
         src_br = bytearray(infile.read(self.READ_SIZE))
 
+
         # this offset indicates where in the source file current payload was read from.
         # this is more like bytes read from the source file before current packet.
-        source_offset = 0
 
         #while len(src_br) == self.READ_SIZE:
         while len(src_br) > 0:
 
             packet = self.make_packet(src_br, source_offset)
+            source_offset += len(src_br)
 
+            # now calculate how many times do we write this packet and where in output it should go.
+            # dont worry about bad disk io pattern, we can implement a decent cached disk io later if the built in
+            # buffering did not perform well.
+
+            # this particular randomizer needs to know the output filesize as part of its calculations
+            # and it thus needs packets all to be the same size (the un-arkiver wont need this assumption tho)
+            if len(packet) < self.MAX_PACKET_SIZE:
+                print "Found short packet. len: " + str(len(packet)) + " I will pad this with 0s"
+                packet.extend( [0 for i in xrange(self.MAX_PACKET_SIZE - len(packet)) ] )
+
+
+            copy1_offset = packets_written * self.MAX_PACKET_SIZE * self.REDUNDANCY_LEVEL
+            outfile.seek(copy1_offset)
             outfile.write(bytes(packet))
-            bytes_written += len(packet)
+
+            print "writing packet# " + str(packets_written) + " redundant copy1 offset is: " + str(copy1_offset)
+
+            # write inverse sequential copy now.
+            copy2_offset = ((total_packet_count - packets_written) * self.REDUNDANCY_LEVEL * self.MAX_PACKET_SIZE) \
+                           - self.MAX_PACKET_SIZE
+
+            outfile.seek(copy2_offset)
+            outfile.write(bytes(packet))
+            print "writing packet# " + str(packets_written) + " redundant copy2 offset is: " + str(copy2_offset)
+
+
+            packets_written += 1
 
             src_br = bytearray(infile.read(self.READ_SIZE))
-            source_offset += len(src_br)
+
+        outfile.flush()
+
+        outfile.seek(0, 2)
+        print "Done. new size is: " + str(outfile.tell()) + " total packets written: " + str(packets_written)
+        print "########"
 
         # done with the loop
         outfile.close()
-
-        print "total bytes written to output file: " + str(bytes_written)
-        return bytes_written
 
 
 
 if __name__ == '__main__':
 
-    print "------------------------------"
+
     rs = RedStream()
 
     rs.redundantize_and_save("../sample_data/test1", "../sample_data/test1.redfile")
-    rs.redundantize_and_save("../sample_data/pic1.jpg", "../sample_data/pic1.jpg.redfile")
+    rs.redundantize_and_save("../sample_data/test2", "../sample_data/test2.redfile")
+    rs.redundantize_and_save("../sample_data/test3", "../sample_data/test3.redfile")
+    rs.redundantize_and_save("../sample_data/test4", "../sample_data/test4.redfile")
+    rs.redundantize_and_save("../sample_data/test5", "../sample_data/test5.redfile")
+    #rs.redundantize_and_save("../sample_data/pic1.jpg", "../sample_data/pic1.jpg.redfile")
